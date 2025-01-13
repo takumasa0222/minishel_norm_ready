7e0192ea65a14a144da6769351acd8fa2f96c708
Takumasa Matsuura <tamatsuu@student.42tokyo.jp>
2025-01-13 18:15:31 +0900

    fixed based on code rabbit comment

--- srcs/map/map_unset.c
+++ srcs/map/map_unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map_unset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ssoeno <ssoeno@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tamatsuu <tamatsuu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 19:54:23 by ssoeno            #+#    #+#             */
-/*   Updated: 2025/01/11 12:03:39 by ssoeno           ###   ########.fr       */
+/*   Updated: 2025/01/13 18:09:02 by tamatsuu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	map_unset(t_map *map, const char *name)
 	t_item	*cur;
 	t_item	*prev;
 
-	if (name == NULL || !is_identifier(name))
+	if (!map || name == NULL || !is_identifier(name))
 		return (ERROR);
 	prev = &map->item_head;
 	cur = map->item_head.next;
