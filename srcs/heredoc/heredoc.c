92cf39405603f885f38ac669d6f03a4a4bfbd808
Shoko Soeno <ssoeno@c6r3s14.42tokyo.jp>
2025-01-19 15:20:23 +0900

    add isatty before rl_event_hook to avoid infinite floop when non tty. refactored input_heredoc_content function

--- srcs/heredoc/heredoc.c
+++ srcs/heredoc/heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tamatsuu <tamatsuu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ssoeno <ssoeno@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 02:14:42 by tamatsuu          #+#    #+#             */
-/*   Updated: 2025/01/19 05:45:00 by tamatsuu         ###   ########.fr       */
+/*   Updated: 2025/01/19 15:18:54 by ssoeno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,72 @@
 #include "../includes/heredoc.h"
 #include "../includes/signals.h"
 
-static int terminate_heredoc(char *line, int *pipe_fds);
+static int	terminate_heredoc(int *pipe_fds);
+int			read_heredoc_line(char *eof, int pipe_fd, t_context *ctx);
+
+// int	input_heredoc_content(char *eof, t_context *ctx)
+// {
+// 	char			*line;
+// 	int				pipe_fds[2];
+
+// 	line = NULL;
+// 	if (isatty(STDIN_FILENO))
+// 		rl_event_hook = heredoc_sigint_event_hook;
+// 	if (pipe(pipe_fds) == -1)
+// 		d_throw_error("input_heredoc_content", "pipe failed");
+// 	while (1)
+// 	{
+// 		free(line);
+// 		line = NULL;
+// 		line = readline("> ");
+// 		if (g_sig == SIGINT)
+// 		{
+// 			ctx->heredoc_interrupted = true;
+// 			return (terminate_heredoc(line, pipe_fds));
+// 		}
+// 		if (line == NULL || \
+// 		(ft_strlen(line) == ft_strlen(eof) && !ft_strcmp(eof, line)))
+// 			break ;
+// 		else
+// 		{
+// 			write(pipe_fds[1], line, ft_strlen(line));
+// 			write(pipe_fds[1], "\n", ft_strlen("\n"));
+// 		}
+// 	}
+// 	if (isatty(STDIN_FILENO))
+// 		rl_event_hook = sigint_event_hook;
+// 	free(line);
+// 	close(pipe_fds[1]);
+// 	return (pipe_fds[0]);
+// }
+/*
+by returning pipe_fds[0], we can redirect STDIN from this pipe
+so that the command reads these lines as input.
+*/
 
 int	input_heredoc_content(char *eof, t_context *ctx)
 {
-	char			*line;
-	int				pipe_fds[2];
+	int	pipe_fds[2];
+	int	ret;
 
-	line = NULL;
-	rl_event_hook = heredoc_sigint_event_hook;
+	if (isatty(STDIN_FILENO))
+		rl_event_hook = heredoc_sigint_event_hook;
 	if (pipe(pipe_fds) == -1)
 		d_throw_error("input_heredoc_content", "pipe failed");
+	ret = read_heredoc_line(eof, pipe_fds[1], ctx);
+	if (isatty(STDIN_FILENO))
+		rl_event_hook = sigint_event_hook;
+	close(pipe_fds[1]);
+	if (ret < 0)
+		return (terminate_heredoc(pipe_fds));
+	return (pipe_fds[0]);
+}
+
+int	read_heredoc_line(char *eof, int pipe_fd, t_context *ctx)
+{
+	char	*line;
+
+	line = NULL;
 	while (1)
 	{
 		free(line);
@@ -35,32 +90,24 @@ int	input_heredoc_content(char *eof, t_context *ctx)
 		if (g_sig == SIGINT)
 		{
 			ctx->heredoc_interrupted = true;
-			return (terminate_heredoc(line, pipe_fds));
+			free (line);
+			return (-2);
 		}
 		if (line == NULL || \
 		(ft_strlen(line) == ft_strlen(eof) && !ft_strcmp(eof, line)))
 			break ;
-		else
-		{
-			write(pipe_fds[1], line, ft_strlen(line));
-			write(pipe_fds[1], "\n", ft_strlen("\n"));
-		}
+		write(pipe_fd, line, ft_strlen(line));
+		write(pipe_fd, "\n", ft_strlen("\n"));
 	}
-	rl_event_hook = sigint_event_hook;
 	free(line);
-	close(pipe_fds[1]);
-	return (pipe_fds[0]);
+	return (EXIT_SUCCESS);
 }
-/*
-by returning pipe_fds[0], we can redirect STDIN from this pipe
-so that the command reads these lines as input.
-*/
 
-static int terminate_heredoc(char *line, int *pipe_fds)
+static int terminate_heredoc(int *pipe_fds)
 {
 	close(pipe_fds[1]);
-	free(line);
-	//rl_event_hook = sigint_event_hook;
+	if (isatty(STDIN_FILENO))
+		rl_event_hook = sigint_event_hook;
 	return (-2);
 }
 
