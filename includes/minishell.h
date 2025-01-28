79caa498b7b582fb67cd39254818cb68b7eb41e8
takumasa0222 <t.tonnbo0222@gmail.com>
2025-01-28 23:54:53 +0900

    refactoring start_exec to meet norm

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tamatsuu <tamatsuu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tamatsuu <tamatsuu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 21:31:55 by shokosoeno        #+#    #+#             */
-/*   Updated: 2025/01/25 18:32:31 by tamatsuu         ###   ########.fr       */
+/*   Updated: 2025/01/28 23:45:25 by tamatsuu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,14 +61,16 @@ typedef struct s_syntax_error {
 	bool	is_error;
 }	t_syntax_error;
 
-void			start_exec(char *line, t_context *ctx);
+// void			start_exec(char *line, t_context *ctx);
 t_context		*init_ctx(void);
 void			clear_ctx(t_context *ctx);
 bool			is_blanc_line(char *line);
 t_syntax_error	*init_syntax_error(void);
+void			start_pipeline(char *line, t_context *ctx);
+t_node			*analyze_pipeline(char *line, t_context *ctx);
 
 void			close_stored_fds(t_context *ctx);
-void	main_loop(t_context *ctx);
-bool	read_command(t_context *ctx);
+void			main_loop(t_context *ctx);
+bool			read_command(t_context *ctx);
 
 #endif
